7f5b77c3711df6342f1d9c7ea655b94a08377320
rahulbollisetty <rahul20bollisetty@gmail.com>
2021-06-20 10:40:47 +0530

    added static scheduling at 12.30hrs and 19.30hrs

--- callapi/scheduler.py
+++ callapi/scheduler.py
@@ -6,8 +6,8 @@ ist = pytz.timezone('Asia/Kolkata')
 from .call import makecall
 def start():
     scheduler = BackgroundScheduler()
+    scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=12, minute=30, timezone=ist)
     scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=19, minute=30, timezone=ist)
-    # scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=17, minute=42, timezone=ist)
     # scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=17, minute=42, timezone=ist)
     scheduler.start()
     print(scheduler.print_jobs())
