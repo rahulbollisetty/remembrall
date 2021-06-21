0a0330cfd6c5451c637d3ab5beed849d3f44a2bf
rahulbollisetty <rahul20bollisetty@gmail.com>
2021-06-21 20:14:42 +0530

    added static scheduling at 12.30hrs and 19.30hrs v5

--- callapi/call.py
+++ callapi/call.py
@@ -3,21 +3,20 @@ import os
 from twilio.rest import Client
 
 
-def makecall():
+def makecall(*args):
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
     account_sid = 'AC256d608ef4ca633250dc030288f03ae6'
     auth_token = '77e8eb5e01a7d2690d36208094a02bc2'
     client = Client(account_sid, auth_token)
-
+    print(args[0])
     call = client.calls.create(
                             url='https://remembroll.herokuapp.com/voice',
-                            to='+918319709976',
+                            to=args[0],
                             from_='+18477449594'
                         )
 
     print(call.sid)
-
 # scheduler = BackgroundScheduler(settings.SCHEDULER_CONFIG)
 # def start():
 #     if settings.DEBUG:
--- callapi/scheduler.py
+++ callapi/scheduler.py
@@ -6,7 +6,8 @@ ist = pytz.timezone('Asia/Kolkata')
 from .call import makecall
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=12, minute=30, timezone=ist)
-    scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=19, minute=30, timezone=ist)
+    scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=12, minute=30, timezone=ist,args=['+918319709976'])
+    scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=19, minute=30, timezone=ist,args=['+918319709976'])
+    scheduler.add_job(makecall,'cron', day_of_week='mon-sun', hour=10, minute=00, timezone=ist,args=['+919100773339'])
     scheduler.start()
     print(scheduler.print_jobs())
